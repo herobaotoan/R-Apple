5531d7460bc3e7ac795f23af19e4b305a07620c4
XcSteven <stevenbui1809@gmail.com>
2023-09-25 15:16:03 +0700

    PV

--- R-GameHub/ProfileView.swift
+++ R-GameHub/ProfileView.swift
@@ -11,34 +11,81 @@
 */
 
 import SwiftUI
+import Firebase
+ import FirebaseStorage
 
 struct ProfileView: View {
-    @Binding var UID: String
-    @StateObject var userViewModel = UserViewModel()
-    @State var name = ""
-    @State var email = ""
-    
-    func show() {
-        self.userViewModel.getUserData()
+    let storage: Storage = Storage.storage()
+         @Binding var UID: String
+         @StateObject var userViewModel = UserViewModel()
+         @State var name = ""
+         @State var email = ""
+         @State var imageURL = ""
+         @State var image: UIImage?
+         @State var shouldShowImagePicker = false
+         func upload() {
+             persistImageToStorage()
+         }
+    private func persistImageToStorage() {
+        let uid = Auth.auth().currentUser!.uid
+        let ref = storage.reference(withPath: uid).child("images/\(Int.random(in: 1..<999999))")
+        guard let imageData = self.image?.jpegData(compressionQuality: 0.5) else { return }
+        ref.putData(imageData, metadata: nil) { metadata, err in
+            if let err = err {
+                //                    self.loginStatusMessage = "Failed to push image to Storage: \(err)"
+                return
+            }
+            ref.downloadURL { url, err in
+                if let err = err {
+                    //                        self.loginStatusMessage = "Failed to retrieve downloadURL: \(err)"
+                    return
+                }
+                imageURL = url?.absoluteString ?? ""
+                userViewModel.updateUserImage(UID: UID, imageURL: imageURL)
+                //                self.loginStatusMessage = "Successfully Added Game"
+            }
+        }
     }
     var body: some View {
-        
         VStack {
-            Text("WELCOME!!")
-                .onAppear() {
-                    show()
-                }
-            ForEach(userViewModel.user, id: \.uid) {user in
-                if (user.documentID == UID) {
-                    Text(user.name)
-                    Text(user.email)
-                    Text(user.phone)
-                    Text(user.imageURL)
-                }
-            }
-            HStack {
-                TextField("Name: ", text: $name)
-                Button {
+             Text("WELCOME!!")
+             ForEach(userViewModel.user, id: \.uid) {user in
+                 if (user.id == UID) {
+                     Text(user.name)
+                     Text(user.email)
+                     Text(user.phone)
+                     Button {
+                         shouldShowImagePicker.toggle()
+                     } label: {
+
+                         if let image = self.image {
+                             Image(uiImage: image)
+                                 .resizable()
+                                 .scaledToFill()
+                                 .frame(width: 128, height: 128)
+                                 .cornerRadius(64)
+                         } else {
+                             AsyncImage(url: URL(string: user.imageURL))
+                                 .scaledToFill()
+                                 .frame(width: 128, height: 128)
+                                 .cornerRadius(64)
+                         }
+                     }
+                     .overlay(RoundedRectangle(cornerRadius: 64)
+                         .stroke(Color.black, lineWidth: 3)
+                     )
+                 }
+             }
+
+             Button {
+                 upload()
+             }label: {
+                 Text("CHANGE AVA")
+             }
+
+             HStack {
+                 TextField("Name: ", text: $name)
+                 Button {
                     userViewModel.updateUserName(UID: UID, name: name)
                 } label: {
                     Text("Change")
@@ -52,10 +99,13 @@ struct ProfileView: View {
                     Text("Change")
                 }
             }
-//            TextField("Image: ", text: $name)
-        }
-        .frame(width: 300)
-    }
+ //            TextField("Image: ", text: $name)
+         }
+         .frame(width: 300)
+         .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+             ImagePicker(image: $image)
+         }
+     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
